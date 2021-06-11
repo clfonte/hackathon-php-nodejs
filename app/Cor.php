8b4eeb9a8057fb006d73e19f2e94d346ea8fb39e
pedroHenriqueMaia <phmaia2018@gmail.com>
2021-06-11 19:27:38 -0300

    insert

--- app/Cor.php
+++ app/Cor.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cor extends Model
 {
-    protected $fillable = ['cor'];
+    protected $fillable = ['cor','update_up','created_up'];
     protected $table = 'cor';
 }
