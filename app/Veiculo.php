aebd0752d32a55aea152da9be48c730c578751af
clfonte <kamilafonte@gmail.com>
2021-06-14 22:51:51 -0300

    Exclusão de models

--- app/Veiculo.php
+++ app/Veiculo.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Veiculo extends Model
 {
-    protected $fillable = ['modelo', 'anoModelo', 'anoFabricacao', 'valor', 'tipo', 'fotoDestaque', 'opcional', 'cor_id', 'marca_id', 'usuario_id'];
+    protected $fillable = ['modelo', 'anoModelo', 'anoFabricacao', 'valor', 'tipo', 'fotoDestaque', 'opcional', 'cor_id', 'marca_id'];
 }
